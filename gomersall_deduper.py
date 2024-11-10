102984debd1e8babf64b90ef86b58ea5f2bed860
Wesley Gomersall <gomersallw@gmail.com>
2024-11-10 09:10:21 -0800

    Removed obsolete functions

--- gomersall_deduper.py
+++ gomersall_deduper.py
@@ -40,42 +40,6 @@ def DNAseqfile_to_set(umilistfile: str, revcomp: bool = False) -> set:
                 print(f"The sequence {barcode} is not a valid DNA sequence, it was not added to the set.")
         return(setofumis) 
 
-def line_info(line: str):
-    """This function reads a (read) line from a sam file. 
-        It outputs information from that line in this order: 
-            Chromosome
-            Adjusted position (accounting for CIGAR string)
-            Barcode (from the end of the QNAME)
-            Bool telling whether the read is aligned to the reverse strand"""
-    splitupline = line.split()
-    chrom = splitupline[2]
-    umi = splitupline[0].split(':')[-1] # barcode is the last section of the first column entry, separated by ':'
-    pos = splitupline[3]
-    rev = int(splitupline[1]) & 16 == 16 
-    cigar = splitupline[5]
-    splitcigar = re.findall(r'\d*\D', cigar) 
-
-    if rev:
-        sum = 0 
-        for i in splitcigar: 
-            if 'M' in i: 
-                sum += int(i.strip('M'))
-            if 'D' in i: # consumes reference (see CIGAR string documentation)
-                sum += int(i.strip('D'))
-            if 'N' in i: # consumes reference (see CIGAR string documentation) 
-                sum += int(i.strip('N'))
-        if 'S' in splitcigar[-1]:
-            sum += int(splitcigar[-1].strip('S'))
-        adjpos = int(pos) + sum - 1
-
-    elif 'S' in splitcigar[0]:    # the read is not reverse complemented 
-        adjpos = int(pos) - int(splitcigar[0].strip('S')) 
-    else: 
-        adjpos = int(pos)
-
-    # print(f"chromosome: {chrom}, pos: {pos}, adjpos: {adjpos}, umi: {umi}, rev: {rev}, cigar: {cigar}, {splitcigar}")
-    return chrom, adjpos, umi, rev
-
 def nearestumi(umi: str, validumis: list, tolerance: int = 2):
     """For some UMI, checks through the list of valid sequences and returns the nearest match (default up to 2 mismatches). 
     If there is no sequence within tolerance, returns 'None'.""" 
@@ -86,118 +50,6 @@ def nearestumi(umi: str, validumis: list, tolerance: int = 2):
             if i == sum(pairwise[0] != pairwise[1] for pairwise in pbases): 
                 return umi_from_list
     return None
-
-def firstduplicate(file_in:str, umis: list, paired_end: bool = False) -> list: 
-    """Returns list of the line numbers for the first of each unique read in SAM file of single-end reads"""
-    readstowrite = list()
-    seenbarcodes = dict()
-    linenum = 0 
-    last_chrom = 'unlikely chromosome name'
-    countbadumi = 0
-    countpcrdup = 0
-    countwritten = 0
-    readsperchrom = dict() 
-    readsthischrom = 0
-
-    with open(INSAM, 'r') as fin: 
-        while True: # writing headers
-            linecontents = fin.readline()
-            linesep = linecontents.split() 
-            if linesep[0] in ['@HD', '@SQ', '@RG', '@PG', '@CO']: # see part 1.3 SAMv1.pdf sam documentation
-                readstowrite.append(linenum)
-                linenum += 1
-            else: 
-                if paired_end:
-                    matecontents = fin.readline()
-                break
-        
-        first_iteration = True  
-        while True:
-
-            written = False
-            if not first_iteration:
-                linecontents = fin.readline()
-                if paired_end: # THIS IS NOT CORRECT!
-                    matecontens = fin.readline()
-
-            if linecontents == "": 
-                readsperchrom.setdefault(last_chrom, readsthischrom)
-                break
-        
-            chrom, adjpos, barcode, revstranded = line_info(linecontents) 
-
-            if paired_end:
-                mate_chrom, mate_adjpos, mate_barcode, mate_revstranded = line_info(matecontents) 
-                corrected_barcode_1 = nearestumi(barcode.split('^')[0], umis)
-                corrected_barcode_2 = nearestumi(barcode.split('^')[1], umis)
-
-                if adjpos < mate_adjpos: # see leftmost samtools markdup documentation
-                    # leftmost = is mate ahead of current read 
-                    orientation = (revstranded, mate_revstranded) 
-                    lineidentifier = f"{adjpos}{mate_adjpos}{orientation}"
-                if adjpos > mate_adjpos:
-                    orientation = (mate_revstranded, revstranded) 
-                    lineidentifier = f"{mate_adjpos}{adjpos}{orientation}"
-                
-                corrected_barcode = corrected_barcode_1 + '^' + corrected_barcode_2 # this is always the orientation due to same QNAME fields
-            
-            else:
-                corrected_barcode_1 = nearestumi(barcode, umis)
-                corrected_barcode_2 = 'Placeholder Barcode'
-                corrected_barcode = corrected_barcode_1
-                lineidentifier = f"{adjpos}:{revstranded}" # key for dict
-
-            if corrected_barcode_1 == None or corrected_barcode_2 == None:
-                last_chrom = chrom
-                if paired_end:
-                    countbadumi += 2
-                else:
-                    countbadumi += 1
-                continue
-
-            if chrom != last_chrom: 
-                written = True # first read on the chromosome must be the first of its kind in the file
-                if not first_iteration:
-                    readsperchrom.setdefault(last_chrom, readsthischrom)
-                    readsthischrom = 0
-                    seenbarcodes.clear()
-    
-            if chrom == last_chrom: 
-                if lineidentifier not in seenbarcodes.keys():
-                    written = True
-                else: 
-                    if corrected_barcode in seenbarcodes[lineidentifier]:
-                        countpcrdup += 1
-                    else: 
-                        written = True # biological duplicate
-
-            if written:
-                countwritten += 1 # should these be 2 for paired end data? 
-                readsthischrom += 1
-                readstowrite.append(linenum) # add line number directly to list of those to write
-                if paired_end: # add mate also 
-                    readstowrite.append(linenum + 1) 
-                # add read to seenbarcodes to be able to detect PCR duplicates
-                if lineidentifier in seenbarcodes.keys():
-                    seenbarcodes[lineidentifier].add(corrected_barcode)
-                else: 
-                    seenbarcodes.setdefault(lineidentifier, set()).add(corrected_barcode) # add key and new set to the table of seen barcodes
-
-            linenum += 1
-            if paired_end:
-                linenum += 1 # skip over mate read
-            first_iteration = False
-            last_chrom = chrom
-
-    print(f"Reads removed due to bad UMIs: {countbadumi}")
-    print(f"PCR duplicates removed: {countpcrdup}")
-    print(f"Reads written to output: {countwritten}")
-
-    print("Chromosome\tNumWritten")
-    for key, value in readsperchrom.items():
-        print(f"{key}\t{value}")
-
-    return readstowrite 
 
 def line_info2(line_num: int, line: str) -> str:
     """This function reads a (read) line from a sam file. 
@@ -521,24 +373,6 @@ def find_dup(filename: str, umiset: set, correction: bool = False, paired: bool 
 
         return readstowrite
 
-# 1. Check one or two barcodes in the QNAME depending on if the reads are paired or not. 
-# 2. If the QNAME is not in the keys of dictionary1 then add it to that dict. 
-    # - The value for this will look like this: "line_number:adjusted_position:reverse(bool):leftmost(bool):length:meanqual" 
-    # - If not paired then leftmost is always false? It shouldnt matter I think.
-# 3. If the QNAME is in the keys of both dicts here then maybe return error here.
-# 4. If the QNAME is in the keys of dictionary1 then add it to a second dictionary: dictionary2
-    # - Same value as above step 2
-# 5. When a chromosome has looped through (this code should be in an if before step 1)
-    # - Depending on if paired data or not create a new dict of either of the following formats:
-    # - Single: {{"adjusted_position:reverse(bool):umi": (line_number, score) ... } ... }
-    # - Paired: {{("R1_adjusted_position:R1_reverse(bool):R1_umi", "R2_adjusted_position:R2_reverse(bool):R2_umi"): (R1line_number, R2line_number, score) ... } ... }
-    # - Score is a new thing: it is the linescore, lengthscore, or qualityscore. These are calculated to determine which read should be written. 
-        # - linescore = lines in file - line_number (this will be largest for the first read encountered). 
-    # - For paired data, use leftmost to determine which dictionary is R1 and which is R2. 
-    # - Now loop through the entries of this dict. For each key we only want the element of the value set/list which has the highest score. Add the line number(s) which are in these to a list.
-    # - The list appended to in this step is that list of lines to write. 
-    # - clear dictionary1 and dictionary2. 
-
 if __name__ == '__main__':
 
     INSAM = get_args().input 
@@ -566,14 +400,6 @@ if __name__ == '__main__':
     CORRECTION = get_args().editumi == 'yes'
 
     writeme = find_dup(INSAM, UMI, CORRECTION, PAIRED, CHOICE)
-        # writeme = firstduplicate(INSAM, UMI, PAIRED)
-
-    # if get_args().choice == 'hi-quality':
-        # writeme = hi_qual_duplicate(INSAM, UMI, PAIRED)
-        # pass
-
-    # if get_args().choice == 'longest':
-        # pass
 
     with open(INSAM, 'r') as fin, open(OUTSAM, 'w') as fout: 
         for linenum, line in enumerate(fin): 
